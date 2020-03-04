7add1bb79e20ccc8ae2a8aa72225ccdec5baf1e0
Jack Fagan <jhfagan579@gmail.com>
2020-03-04 13:19:16 -0600

    cleaned up code

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,8 +1,6 @@
 ﻿namespace LoggingKata
 {
-    /// <summary>
-    /// Parses a POI file to locate all the Taco Bells
-    /// </summary>
+  
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
@@ -19,29 +17,16 @@
             //DONE// Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
             var cells = line.Split(',');
 
-            //DONE// If your array.Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
-                //DONE// Log that and return null
                 logger.LogError("Incorrect Input Info");
-                //DONE// Do not fail if one record parsing fails, return null
-                return null; // TODO Implement
+                return null;
             }
-            
-            //DONE// grab the latitude from your array at index 0
+
             var lat = double.Parse(cells[0]);
-            //DONE// grab the longitude from your array at index 1
             var longi = double.Parse(cells[1]);
-            //DONE// grab the name from your array at index 2
             var name = cells[2];
-            //DONE// Your going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
 
-            //DONE// You'll need to create a TacoBell class
-            //DONE// that conforms to ITrackable
-
-            //DONE// Then, you'll need an instance of the TacoBell class
-            //DONE// With the name and point set correctly
             var p = new Point()
             {
               Latitude = lat,
@@ -52,11 +37,7 @@
             tacoBell.lat = lat;
             tacoBell.longi = longi;
             tacoBell.Name = name;
-            //var tacoBell = new TacoBells(name, lat, longi);
-            //DONE// Then, return the instance of your TacoBell clas
-            //DONE// Since it conforms to ITrackable
-
-           
+ 
             return tacoBell;
             
         }
